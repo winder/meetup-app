7b009f25aa989e7f88884395751cfedeb1db2d63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-16 22:13:01 +0000

    Deploy winder/meetup-app to github.com/winder/meetup-app.git:gh-pages

---
+++ precache-manifest.7a65e32976a5548a616ac214f3c331d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d395ae2031644872d21d52e80462d6",
+    "url": "/meetup-app/index.html"
+  },
+  {
+    "revision": "aa93c2b11183f77634e7",
+    "url": "/meetup-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/meetup-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "aa93c2b11183f77634e7",
+    "url": "/meetup-app/static/js/main.173aeb3a.chunk.js"
+  },
+  {
+    "revision": "45d549a9594041d799ca",
+    "url": "/meetup-app/static/js/runtime~main.fb9ef33d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
